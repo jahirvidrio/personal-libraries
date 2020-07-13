de8defc2a98f58a43e3ac6c7762a156651c9db82
Jahir Vidrio <jahir.vidrio@gmail.com>
2020-07-12 22:54:21 -0500

    refactorized functions definition

--- ReactJS/useMediaQuery.js
+++ ReactJS/useMediaQuery.js
@@ -2,11 +2,11 @@ import { useState, useEffect } from 'react';
 
 
 /**
- * 
- * @param {string} expresion : min-width | max-width | min-height | max-height 
+ *
+ * @param {string} expresion : min-width | max-width | min-height | max-height
  * @param {number} size : >= 0
  * @param {string} [unit = px] : px | em | rem
- * 
+ *
  */
 
 
@@ -21,7 +21,7 @@ function useMediaQuery(expresion, size, unit = 'px') {
     mediaQuery.addListener(mediaQueryListener);
     mediaQueryListener();
 
-    return () => mediaQuery.removeEventListener(mediaQueryListener);
+    return () => mediaQuery.removeListener(mediaQueryListener);
   }, []);
 
   return match;
